34bb4f12590618f2a4b65570b8f459993e430a72
Alec Zaitz <a02423683@usu.edu>
2025-10-08 09:12:18 -0600

    fixed login issues

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './login.css';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+        <section className="jorvoHeader">
+            <img src="JorvoLogo.png" alt="Jorvo Logo"/>
+            <section className="loginSection">
+                <h2>Login</h2>
+                <form className ="loginForm" action="/home">
+                    <input type ="text" id="username" name="username" placeholder="Username" required />
+                    <input type ="password" id="password" name="password" placeholder="Password" required />
+                    <button type="submit">Login</button>
+                </form>
+            </section>
+        </section>
     </main>
   );
 }
