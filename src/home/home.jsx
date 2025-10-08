39e0c468c38738b19dcda8f6853af2ae063bc928
Alec Zaitz <a02423683@usu.edu>
2025-10-08 11:36:54 -0600

    fixed styling for home page

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import './home.css';
 
 export function Home() {
   return (
     <main>
         <section className="matchStartSection">
             <h2>Join or start a match</h2>
-            <input type="text" className="matchCodeInput" placeholder="Enter match Code"/>
-            <button className="startButton">Join Match</button>
+            <div className="matchCodeInput">
+              <input type="text" placeholder="Enter match Code"/>
+              <button className="startButton">Join Match</button>
+            </div>
             <button className="newMatchButton">Start a new match</button>
         </section>
-        <section>
-            <h3>Past Matches</h3>
+        <section className="pastMatchesSection">
+            <h2>Past Matches</h2>
             <ul>
                 <li className="matchItem">match, winner, and speed <button>Rematch</button></li>
                 <li className="matchItem">match, winner, and speed <button>Rematch</button></li>
