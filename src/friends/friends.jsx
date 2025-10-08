888887ab8722f28a3feba8ec063cb9b771cd31b4
Alec Zaitz <a02423683@usu.edu>
2025-10-08 11:59:44 -0600

    fixed styling for friends page

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import './friends.css';
 
 export function Friends() {
   return (
         <main>
-            <section>
-                <h2>Friends</h2>
-                <input type="text" className="friendNameInput" placeholder="Enter Friend's Name"/>
-                <button className="addFriendButton">Add Friend</button>
-                <p>A list of added friends will appear hear</p>
+            <h2>Friends</h2>
+            <section className="addFriendSection">
+                <input type="text" placeholder="Enter Friend's Name" className="friendInput"/>
+                <button className="accentButton">Add Friend</button>
+            </section>
+            <section className="friendListSection">
+              <h2>Current Friends</h2>
+              <ul>
+                  <li className="friendItem">Friend Name <button className="primaryButton">Start Match</button></li>
+                  <li className="friendItem">Friend Name <button className="primaryButton">Start Match</button></li>
+                  <li className="friendItem">Friend Name <button className="primaryButton">Start Match</button></li>
+              </ul>
             </section>
         </main>
   );
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -8,16 +8,16 @@ export function Home() {
             <h2>Join or start a match</h2>
             <div className="matchCodeInput">
               <input type="text" placeholder="Enter match Code"/>
-              <button className="startButton">Join Match</button>
+              <button className="primaryButton">Join Match</button>
             </div>
-            <button className="newMatchButton">Start a new match</button>
+            <button className="accentButton">Start a new match</button>
         </section>
         <section className="pastMatchesSection">
             <h2>Past Matches</h2>
             <ul>
-                <li className="matchItem">match, winner, and speed <button>Rematch</button></li>
-                <li className="matchItem">match, winner, and speed <button>Rematch</button></li>
-                <li className="matchItem">match, winner, and speed <button>Rematch</button></li>
+                <li className="matchItem">match, winner, and speed <button className="primaryButton">Rematch</button></li>
+                <li className="matchItem">match, winner, and speed <button className="primaryButton">Rematch</button></li>
+                <li className="matchItem">match, winner, and speed <button className="primaryButton">Rematch</button></li>
             </ul>
         </section>
     </main>
