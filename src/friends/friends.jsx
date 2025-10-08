08840a98f119af60d27929835232b87f9d691b0b
Alec Zaitz <a02423683@usu.edu>
2025-10-08 09:16:31 -0600

    converted the friends page

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export function Friends() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>friends displayed here</div>
-    </main>
+        <main>
+            <section>
+                <h2>Friends</h2>
+                <input type="text" id="friendNameInput" placeholder="Enter Friend's Name"/>
+                <button id="addFriendButton">Add Friend</button>
+                <p>A list of added friends will appear hear</p>
+            </section>
+        </main>
   );
 }
