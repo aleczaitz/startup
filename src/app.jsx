e7c2e71111b907e01704cfe91257d915652a7a5a
Alec Zaitz <a02423683@usu.edu>
2025-10-08 08:38:03 -0600

    added the router

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+
 
 export default function App() {
   return (
-    <div className="body">
-        <header>
-            Jorvo
-        </header>
-    </div>
+    <BrowserRouter>
+        <div className="body">
+            <header className="container-fluid">
+                <nav className="navbar fixed-top">
+                    <div className="navbar-brand">
+                        Jorvo
+                    </div>
+                    <menu className="navbar-brand">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Login</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/home">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/friends">Friends</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/about">About</NavLink>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/home' element={<Play />} />
+                <Route path='/friends' element={<Scores />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </div>
+    </BrowserRouter>
   )
 }
+
+function NotFound() {
+    return (
+        <main className="container-fluid bg-secondary text-center">
+        <div>404 - Not Found</div>
+        </main>
+    );
+    }
