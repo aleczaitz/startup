d07cfaab46cd90b5bc46631890fbef70884b39ed
Alec Zaitz <aleczaitz@gmail.com>
2025-11-04 21:46:25 -0700

    finished user creation

--- service/index.js
+++ service/index.js
@@ -1,11 +1,15 @@
 const port = process.argv.length > 2 ? process.argv[2] : 4000; // Default port is 4000, otherwise use the provided argument
-
 const express = require('express');
 const app = express();
-
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const cookieParser = require('cookie-parser');
+
+// This will be kept in server RAM and be deleted once the server restarts
+const users = [];
+
+// The field that keeps the auth token on the client browser
+const authCookieName = 'token'
 
 // This causes Express static middleware to serve files from the pulic directory
 app.use(express.static('public'));
@@ -26,13 +30,28 @@ apiRouter.post('/auth/create', async (req, res) => {
 
 // Helper functions
 
-function createUser() {
+async function createUser(email, password) {
     // create a user object and store it in the server RAM for now
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+    return user;
 }
 
-function setAuthCookie() {
+function setAuthCookie(res, user) {
     // set the response cookie to store an auth token in the user's browser
     // and store the token in the server RAM
+    res.cookie(authCookieName, authToken, {
+        maxAge: 1000 * 60 * 60, // How long the cookie lasts in the browser in ms
+        secure: true, // only sent over https?
+        httpOnly: true, // doesn't show up in js (document.cookie)
+        sameSite: 'strict', // doesn't allow cross origin attacks
+    });
 }
 
 
